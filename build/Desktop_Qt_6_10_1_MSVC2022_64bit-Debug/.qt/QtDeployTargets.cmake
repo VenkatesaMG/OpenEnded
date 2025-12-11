4748bbf5d49d3aa53903db7a711d1ff108ff678a
VenkatesaMG <m.g.venkatesa@gmail.com>
2025-12-11 22:28:59 +0530

    CHORE: Remove generated build files from repository.

--- build/Desktop_Qt_6_10_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_OpenEnded_FILE C:/Users/mgven/Downloads/OpenEnded/build/Desktop_Qt_6_10_1_MSVC2022_64bit-Debug/OpenEnded.exe)
-set(__QT_DEPLOY_TARGET_OpenEnded_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_OpenEnded_RUNTIME_DLLS C:/Qt/6.10.1/msvc2022_64/bin/Qt6PdfWidgetsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Pdfd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.1/msvc2022_64/bin/Qt6Cored.dll)
